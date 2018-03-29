8c983c8d0b24de122af252d198880f66bac3b88f
badr aldeen shek salim <baldeen.sheksalim@std.hku.edu.tr>
2018-03-29 20:26:07 +0300

    FIRST BUILD

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Upload your  Website | The Best website Uploader</title>
+    <title>Upload your  Website  | The Best website Uploader</title>
  
 <meta property="og:url"                content="upload-website.com" />
 <meta property="og:type"               content="article" />
