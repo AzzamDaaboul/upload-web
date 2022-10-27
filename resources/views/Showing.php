d5faea6df4c8df14fb04294c53f493544898f924
Azzam Daaboul <azzamdaaboul@gmail.com>
2022-10-27 12:16:04 +0200

    Update Showing.php

--- resources/views/Showing.php
+++ resources/views/Showing.php
@@ -4,6 +4,8 @@
   <title>Bootstrap Example</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  		<title>{{ config('app.name') }} - {{ config('app.subtitle') }}</title>
+
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
