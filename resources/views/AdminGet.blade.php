bf55e78e6358735da6fc824025e64afa41855c76
Azzam Daaboul <azzamdaaboul@gmail.com>
2022-10-27 12:17:43 +0200

    Update AdminGet.blade.php

--- resources/views/AdminGet.blade.php
+++ resources/views/AdminGet.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
-    
+    		<title>{{ config('app.name') }} - {{ config('app.subtitle') }}</title>
+
      <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
